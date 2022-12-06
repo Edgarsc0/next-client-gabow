0a699a9971788271275246bd859592511fddb390
Edgarsc0 <esanchezc2004@alumno.ipn.mx>
2022-12-06 01:34:56 -0600

    nueva act

--- pages/Edificio/[place].js
+++ pages/Edificio/[place].js
@@ -1,9 +1,23 @@
 import { useRouter } from 'next/router'
+import { useState } from 'react';
+import places from "../../public/cecyt9.json";
 const Edificio=()=>{
     const router=useRouter();
     const {place}=router.query;
+    const [piso,setPiso]=useState("PB");
     return(
-        <h1>Hola {place}</h1>
+        <>
+            {place}
+            <SVG place={place} piso={piso}/>
+        </>
     )
+}
+function SVG({place,piso}){
+    if(place=="CECyT9"){
+        switch(piso){
+            case "PB":
+                return places.edifAulas.plantaBaja;
+        }
+    }
 }
 export default Edificio;
