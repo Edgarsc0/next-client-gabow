0c8cc82cf5d2e6ea68c6f981d8cc5ee30e3dd09b
Edgarsc0 <esanchezc2004@alumno.ipn.mx>
2022-12-07 03:15:02 -0600

    nueva act

--- pages/api/db/querys.js
+++ pages/api/db/querys.js
@@ -2,7 +2,8 @@ const myQuerys={
     selectPlaces:"select * from Espacio_general",
     selectLugares:"select distinct cod_lugar from codigo_espgeneral where esg_nombre=?",
     selectInnerJoinTable:"select * from codigo_espgeneral",
-    selectPisos:"select cod_piso from codigo_espgeneral where esg_nombre=? and cod_lugar=?"
+    selectPisos:"select cod_piso from codigo_espgeneral where esg_nombre=? and cod_lugar=?",
+    selectSVG:"select convert(cod_codigo using utf8) as svg from codigo_espgeneral where esg_nombre=? and cod_piso=? and cod_lugar=?"
 }
 
 export default myQuerys;
