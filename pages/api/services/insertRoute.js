82696db854588a82c261e70d44c518c167c67f49
Edgarsc0 <esanchezc2004@alumno.ipn.mx>
2022-12-09 12:39:38 -0600

    nueva act

--- pages/api/services/insertRoute.js
+++ pages/api/services/insertRoute.js
@@ -4,6 +4,7 @@ export default function(req,res){
     const {rut_origen,rut_destino,usu_correo,id_fecha,id_espg}=req.body;
     con.query(myQuerys.insertRoute,[rut_origen,rut_destino,usu_correo,id_fecha,id_espg],(err,result)=>{
         if(err){
+            console.log(err);
             return res.json({
                 status:"Something went wrong"
             });
