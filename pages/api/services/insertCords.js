62a4ba14f28f50637c02b0ffe450b7ef0f3f7d99
Edgarsc0 <esanchezc2004@alumno.ipn.mx>
2022-12-09 13:36:06 -0600

    nueva act

--- pages/api/services/insertCords.js
+++ pages/api/services/insertCords.js
@@ -3,20 +3,18 @@ import myQuerys from "../db/querys";
 export default function(req,res){
     const {cords,id_ruta}=req.body;
     console.log(cords);
-    let i=0;
     cords.forEach(item=>{
         console.log(item);
-        console.log(i);
         con.query(myQuerys.insertCords,[item[0],item[1],id_ruta],(err,result)=>{
             if(err){
                 console.log(err);
                 return res.json({
                     status: "something went wrong"
                 });
-            }else{
-                i++;
             }
         });
-        console.log(i==cords.length);
+    });
+    return res.json({
+        status:"ok"
     });
 }
