d2abfe51417de4fc65bf57ef296b7d866cdf8eb0
Edgarsc0 <esanchezc2004@alumno.ipn.mx>
2022-12-12 02:51:59 -0600

    nueva act

--- pages/api/db/querys.js
+++ pages/api/db/querys.js
@@ -7,7 +7,8 @@ const myQuerys={
     selectPlaceById:"select esg_nombre from codigo_espgeneral where convert(cod_codigo using utf8) like ?",
     insertDate:"insert into Ruta_fecha(rfc_dia,rfc_mes,rfc_anno) values(?,?,?)",
     insertRoute:"insert into Ruta(rut_origen,rut_destino,usu_correo,id_fecha,id_espg) values(?,?,?,?,?)",
-    insertCords:"insert into Coordenadas(cor_latitud,cor_longitud,id_ruta) values(?,?,?)"
+    insertCords:"insert into Coordenadas(cor_latitud,cor_longitud,id_ruta) values(?,?,?)",
+    selectRoutes:"select * from ruta_fecha_espg where usu_correo=?",
 }
 
 export default myQuerys;
--- pages/api/services/getRoutes.js
+++ pages/api/services/getRoutes.js
@@ -1,6 +1,18 @@
+import con from "../db/config";
+import myQuerys from "../db/querys";
 export default function(req,res){
-    console.log("hola");
-    return res.json({
-        status:"ok"
+    const {user}=req.body;
+    con.query(myQuerys.selectRoutes,[user],(err,result)=>{
+        if(err){
+            return res.json({
+                status: "something went wrong",
+                error:err
+            });
+        }else{
+            return res.json({
+                status:"ok",
+                info:result
+            });
+        }
     });
 }
