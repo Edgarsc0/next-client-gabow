c0b6d851112d01b1be1b86d7a2b34a4956ec99ec
Edgarsc0 <esanchezc2004@alumno.ipn.mx>
2022-12-11 22:55:24 -0600

    nueva act

--- pages/See3D.jsx
+++ pages/See3D.jsx
@@ -1,40 +1,41 @@
 import React, { Suspense } from "react";
 import {Canvas} from 'react-three-fiber';
 import { Environment, OrbitControls , Sky } from "@react-three/drei";
-import { Piso33D } from "../components/Piso33D";
+import { Piso3D } from "../components/Piso3D";
 import Select from 'react-select'
 import * as THREE from 'three';
+import { useState } from "react";
 
 const options = [{
 
     lavel : 'Piso 3',
-    value : 'Piso 3'
+    value : 'Piso3.glb'
 
 },
 {
 
     lavel : 'Piso 2',
-    value : 'Piso 2'
+    value : 'Piso2.glb'
 
 }
 ];
 
-const change3dView = (selectedOption) =>{
 
-    console.log(selectedOption);
-
-}
 
 const See3D = () => {
-
+    const [search,setSearch]=useState();
+    const change3dView = (selectedOption) =>{
+        console.log(selectedOption.value);
+        setSearch(selectedOption.value);
+    }
     return(
         <>
-            <Select options={options} onChange={change3dView}/>
+            <Select options={options} onChange={change3dView} placeholder='Pisos'/>
             <div style={{ width: '500px' , height:'500px' , margin: '10px auto'}}>
                 <Canvas camera={{zoom : .85 , position : [-100 , 250 , -240]}}>
                 <ambientLight intensity={.3} />
                 <Suspense>
-                    <Piso33D/>
+                    <Piso3D selected={search}/>
                 </Suspense>
                 <OrbitControls/>
                 <Environment background>
