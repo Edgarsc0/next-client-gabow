38bf6ff16cd201a4e412229c8e1556f322c787e1
Edgarsc0 <esanchezc2004@alumno.ipn.mx>
2022-12-12 02:19:03 -0600

    nueva act

--- pages/MisRutas.jsx
+++ pages/MisRutas.jsx
@@ -1,14 +1,26 @@
+import axios from 'axios';
+import { useEffect } from 'react';
 import { useState } from 'react';
 import styles from '../styles/Mapas.module.scss'
 export default function MisRutas(){
     const [cords,setCords]=useState([]);
+    const [email,setEmail]=useState();
+    const getEmail=async()=>{
+        const {data}=await axios.post("/api/auth/getCookie");
+        setEmail(data.token.email);
+    }
+    const getCords=async()=>{
+        const {data}=await axios.post("/api/services/getRoutes");
+    }
+    useEffect(()=>{
+        getEmail();
+    });
     return(
         <>
             <div className={styles.container3}>
-                <h1>Mis Rutas</h1>
+                <h1>Mis Rutas: {email}</h1>
             </div>
             <hr></hr>
-
         </>
     )
 }
