7bb57d80171fe0d3c65e0886a10e709287c19bb4
Edgarsc0 <esanchezc2004@alumno.ipn.mx>
2022-12-12 23:59:17 -0600

    nueva act

--- pages/Favoritos.jsx
+++ pages/Favoritos.jsx
@@ -3,8 +3,25 @@ import Head from 'next/head'
 import Link from 'next/link'
 import styles from '../styles/Favoritos.module.scss'
 import Script from 'next/script'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import axios from 'axios'
 
 const Favoritos = () => {
+    const [favs,setFavs]=useState([]);
+    const getFavs=async()=>{
+        const {data}=await axios.post("/api/services/getFavs");
+        if(data.status=="ok"){
+            setFavs(data.favs);
+        }else{
+            window.location.href="/Mapas";
+        }
+    }
+    useEffect(()=>{
+        if(!favs){
+            getFavs();
+        }
+    });
     return (
         <>
             {/* <Head>
@@ -13,38 +30,22 @@ const Favoritos = () => {
             <div className={styles.container}>
                 <table className={styles.tabla}>
                     <tbody>
-                        <tr>
-                            <td className={styles.celda}>Town Center</td>
-                            <td className={styles.celda}>Cinepolis</td>
-                            <td className={styles.celda}>
-                                <Link href="/Ruta"><button type="button" className={styles.button}>Iniciar ruta</button></Link>
-                                <button type="button" className={styles.button}><lord-icon src="https://cdn.lordicon.com/kfzfxczd.json" trigger="hover" colors="primary:#ffffff" width="32px" height="32px"></lord-icon></button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className={styles.celda}>Town Center</td>
-                            <td className={styles.celda}>Walmart</td>
-                            <td className={styles.celda}>
-                                <Link href="/Ruta"><button type="button" className={styles.button}>Iniciar ruta</button></Link>
-                                <button type="button" className={styles.button}><lord-icon src="https://cdn.lordicon.com/kfzfxczd.json" trigger="hover" colors="primary:#ffffff" width="32px" height="32px"></lord-icon></button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className={styles.celda}>CECyT 9</td>
-                            <td className={styles.celda}>Aula 4.0</td>
-                            <td className={styles.celda}>
-                                <Link href="/Ruta"><button type="button" className={styles.button}>Iniciar ruta</button></Link>
-                                <button type="button" className={styles.button}><lord-icon src="https://cdn.lordicon.com/kfzfxczd.json" trigger="hover" colors="primary:#ffffff" width="32px" height="32px"></lord-icon></button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className={styles.celda}>CECyT 9</td>
-                            <td className={styles.celda}>Salon 13</td>
-                            <td className={styles.celda}>
-                                <Link href="/Ruta"><button type="button" className={styles.button}>Iniciar ruta</button></Link>
-                                <button type="button" className={styles.button}><lord-icon src="https://cdn.lordicon.com/kfzfxczd.json" trigger="hover" colors="primary:#ffffff" width="32px" height="32px"></lord-icon></button>
-                            </td>
-                        </tr>
+                        {favs.map(item=>(
+                            <tr key={item.lugar}>
+                                <td className={styles.celda}>{item.place}</td>
+                                <td className={styles.celda}>{item.lugar}</td>
+                                <td>
+                                    <Link href={`/Ruta/${item.lugar}`}>
+                                        <button type="button" className={styles.button}>Iniciar ruta</button>
+                                    </Link>
+                                    <Link href={`/EliminarFavs/${item.lugar}`}>
+                                        <button type="button" className={styles.button}>
+                                            <lord-icon src="https://cdn.lordicon.com/kfzfxczd.json" trigger="hover" colors="primary:#ffffff" width="32px" height="32px"></lord-icon>
+                                        </button>
+                                    </Link>
+                                </td>
+                            </tr>
+                        ))}
                     </tbody>
                 </table>
             </div>
--- pages/api/services/getFavs.js
+++ pages/api/services/getFavs.js
@@ -1,3 +1,13 @@
 export default function(req,res){
-    
+    const {favs}=req.cookies;
+    if(favs){
+        return res.json({
+            status:"ok",
+            favs:JSON.parse(favs)
+        })
+    }else{
+        return res.json({
+            status:"no cookie favs"
+        });
+    }
 }
