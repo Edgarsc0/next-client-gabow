c2f09cef6216c29c1ef1a4e32f697f0263e66784
Edgarsc0 <esanchezc2004@alumno.ipn.mx>
2022-12-12 18:40:04 -0600

    nuevaa ct

--- pages/api/services/validateId.js
+++ pages/api/services/validateId.js
@@ -2,12 +2,17 @@ import con from "../db/config";
 import myQuerys from "../db/querys";
 export default function(req,res){
     const {id,email}=req.body;
-    console.log(id,email);
-    return res.json({
-        status:"ok",
-        data:{
-            id:id,
-            email:email
+    con.query(myQuerys.selectRoutes,[email],(err,result)=>{
+        if(err){
+            return res.json({
+                status:"something went wrong",
+                error:err
+            });
+        }else{
+            return res.json({
+                status:"ok",
+                info:result.find(item=>item.id_ruta=id),
+            });
         }
-    })
+    });
 }
