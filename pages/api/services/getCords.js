7eb34502969625bef13c91441bc445bfbdc9bd2c
Edgarsc0 <esanchezc2004@alumno.ipn.mx>
2022-12-13 02:25:58 -0600

    nueva act

--- pages/api/services/getCords.js
+++ pages/api/services/getCords.js
@@ -1,5 +1,6 @@
 import con from "../db/config";
 import myQuerys from "../db/querys";
+const arrayPosition=[];
 export default function(req,res){
     const {id}=req.body;
     con.query(myQuerys.selectCords,[id],(err,result)=>{
@@ -8,9 +9,12 @@ export default function(req,res){
                 status:"something went wrong"
             });
         }else{
+            result.map(item=>{
+                arrayPosition.push([item.cor_latitud,item.cor_longitud])
+            });
             return res.json({
                 status:"ok",
-                response:result
+                cords:arrayPosition
             })
         }
     });
