74789c80eca202a29d59ad1c0b9f5a7d0fa51767
Edgarsc0 <esanchezc2004@alumno.ipn.mx>
2023-02-06 15:24:10 -0600

    nueva act

--- pages/api/db/config.js
+++ pages/api/db/config.js
@@ -5,11 +5,11 @@ import CryptoJS from "crypto-js";
 dotenv.config();
 
 const con=mysql.createPool({
-    host: CryptoJS.AES.decrypt(process.env.BD_HOST,process.env.SECURE_KEY).toString(CryptoJS.enc.Utf8),
-    port: CryptoJS.AES.decrypt(process.env.BD_PORT,process.env.SECURE_KEY).toString(CryptoJS.enc.Utf8),
-    password: CryptoJS.AES.decrypt(process.env.BD_PASSWORD,process.env.SECURE_KEY).toString(CryptoJS.enc.Utf8),
-    user: CryptoJS.AES.decrypt(process.env.BD_USER,process.env.SECURE_KEY).toString(CryptoJS.enc.Utf8),
-    database: CryptoJS.AES.decrypt(process.env.BD_DATABASE,process.env.SECURE_KEY).toString(CryptoJS.enc.Utf8)
+    host: CryptoJS.AES.decrypt(process.env.BD_HOST,process.env.SECRET_KEY).toString(CryptoJS.enc.Utf8),
+    port: CryptoJS.AES.decrypt(process.env.BD_PORT,process.env.SECRET_KEY).toString(CryptoJS.enc.Utf8),
+    password: CryptoJS.AES.decrypt(process.env.BD_PASSWORD,process.env.SECRET_KEY).toString(CryptoJS.enc.Utf8),
+    user: CryptoJS.AES.decrypt(process.env.BD_USER,process.env.SECRET_KEY).toString(CryptoJS.enc.Utf8),
+    database: CryptoJS.AES.decrypt(process.env.BD_DATABASE,process.env.SECRET_KEY).toString(CryptoJS.enc.Utf8)
     /*host:"localhost",
     port:3306,
     password:"Hal02012()",
