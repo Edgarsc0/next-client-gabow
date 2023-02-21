ec9a9805061d5ed40e7734af8e044ce1fcc1e3f4
Onyxmoises <99691557+Onyxmoises@users.noreply.github.com>
2023-02-21 14:40:20 -0600

    Revert "Prueba menu"
    This reverts commit 0e7897b535200db8ebb1ae16c030f474f33bef62.

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -8,8 +8,6 @@ import Info from './Info'
 import Inicio from './Inicio'
 import Ley from './Ley'
 
-import Info2 from './Info2'
-
 const Menu = () => {
     // const [data,setData]=useState([]);
     // useEffect(()=>{
@@ -102,7 +100,6 @@ const Menu = () => {
 
                     {/* COMPONENTE INFO */}
                     <Info/>
-                    <Info2/>
 
                 </div>
 
