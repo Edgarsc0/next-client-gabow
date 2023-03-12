8154317b96a1b99ee46d8262d5948a82b75ea97c
Edgarsc0 <esanchezc2004@alumno.ipn.mx>
2023-03-12 15:40:26 -0600

    nueva act

---
+++ pages/test/GoogleAuth.jsx
@@ -0,0 +1,7 @@
+import AuthGoogleButton from "../../components/AuthGoogleButton";
+const GoogleAuth=()=>{
+    return(
+        <AuthGoogleButton></AuthGoogleButton>
+    )
+}
+export default GoogleAuth;
