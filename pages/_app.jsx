929b69b8e33d16c298cf6257fef480ce63dc1c1d
Edgarsc0 <esanchezc2004@alumno.ipn.mx>
2023-03-11 22:09:34 -0600

    prueba2 google auth

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,13 +3,17 @@ import Menu from '../components/Menu'
 // import Animacion from '../components/Animacion'
 // import Footer from '../components/Footer'
 import {Analytics} from "@vercel/analytics/react"
-
-function MyApp({ Component, pageProps }) {
-    return <>
-      <Menu />
-      <Component {...pageProps} />
-      <Analytics/>
-    </>
+import { SessionProvider } from 'next-auth/react'
+function MyApp({ Component,pageProps,session }) {
+    return (
+      <SessionProvider session={session}>
+        <>
+          <Menu/>
+          <Component {...pageProps} />
+          <Analytics/>
+        </>
+      </SessionProvider>
+    )
 }
 
 export default MyApp
