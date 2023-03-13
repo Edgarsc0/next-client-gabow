5fa637e35229512993b7c211ef987ebf0b81907f
Edgarsc0 <esanchezc2004@alumno.ipn.mx>
2023-03-12 18:46:25 -0600

    nueva act

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,16 +2,17 @@ import '../styles/globals.scss'
 import Menu from '../components/Menu'
 // import Animacion from '../components/Animacion'
 // import Footer from '../components/Footer'
-import {Analytics} from "@vercel/analytics/react"
-import { SessionProvider } from 'next-auth/react'
-function MyApp({ Component,pageProps,session }) {
-    return (
-      <SessionProvider session={session}>
-          <Menu/>
-          <Component {...pageProps} />
-          <Analytics/>
-      </SessionProvider>
-    )
+import { Analytics } from "@vercel/analytics/react"
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Menu />
+      <Component {...pageProps} />
+      <Analytics />
+
+    </>
+  )
 }
 
 export default MyApp
