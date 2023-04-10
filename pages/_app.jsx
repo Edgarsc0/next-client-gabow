ff2e61d5ce9eb4c7935234ce94e4ed6f0dc7ca75
Edgarsc0 <esanchezc2004@alumno.ipn.mx>
2023-04-10 16:41:08 -0600

    nueva act

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,17 +2,16 @@ import '../styles/globals.scss'
 import Menu from '../components/Menu'
 // import Animacion from '../components/Animacion'
 // import Footer from '../components/Footer'
-import { Analytics } from "@vercel/analytics/react"
 
-function MyApp({ Component, pageProps }) {
+
+function App({ Component, pageProps }) {
   return (
     <>
       <Menu />
       <Component {...pageProps} />
-      <Analytics />
 
     </>
   )
 }
 
-export default MyApp
+export default App
